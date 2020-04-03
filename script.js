640607cb30745ba7c2b74c45ea4313c83b8cbbf5
AkiyoS <a.shikakura@gmail.com>
2020-04-04 05:21:36 +0900

    tommyさんのwebページ

--- script.js
+++ script.js
@@ -12,4 +12,26 @@ $(function () {
         $('.modal > button').toggleClass('tg');
     });
 
+    $(function () {
+        var $topBtn = $(".topBtn")
+        $topBtn.hide();
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 500) {
+                $topBtn.fadeIn();
+            } else {
+                $topBtn.fadeOut();
+            }
+        });
+        $('a[href^="#"]').click(function () {
+            var href = $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top;
+            $("html, body").animate({
+                scrollTop: position
+            }, 500, "swing");
+            return false;
+        });
+    });
+
+
 });
